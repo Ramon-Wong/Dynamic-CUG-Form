dd0ec2e4871aa4db5e1bd6cb8c0ae0f9a2bdfada
Ramon Wong <rcnwong78@gmail.com>
2014-06-20 16:15:14 -0300

    Create style.js

---
+++ style.js
@@ -0,0 +1,17 @@
+var style1        = {width:           "950px", 
+                     backgroundColor: "#FFFFFF",
+                     border:          "1px solid #333333",
+                     borderRadius:    "4px",
+                     margin:          "15px auto",
+                     padding:         "5px 14px"
+                    };
+
+
+var Title         = {fontSize:       "23px",
+                     fontFamily:     "Arial Narrow,Arial,Helvetica,sans-serif",
+                     padding:        "10px 0"
+                    };
+
+var style2        = {margin:         " 0 0 0 30px"
+  
+                    };
